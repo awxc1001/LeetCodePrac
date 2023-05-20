f9a7db9f034a40b42f2d7e7c947a6c86c3263d70
Andre Wang <110853339+awxc1001@users.noreply.github.com>
2023-05-20 19:41:54 +1000

    Time: 2 ms (80.31%), Space: 42.1 MB (98.95%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    LinkedList<String> path = new LinkedList<>();
+    LinkedList<String> res = new LinkedList<>();
+    
+    public List<String> binaryTreePaths(TreeNode root) {
+        
+        traverse(root);
+        return res;
+            
+    }
+    
+    public void traverse(TreeNode root){
+        if(root == null){
+            return;
+        }
+        
+        //preorder
+        //convert value to string
+        path.addLast(root.val + "");
+        
+        traverse(root.left);
+        traverse(root.right);
+        
+        //if leafNode, this can postOrder or preOrder, preOrder faster 
+        if(root.left == null && root.right == null){
+            // add to result
+            res.addLast(String.join("->",path));
+            // always remove the path
+            path.removeLast();
+            return;
+        }
+        path.removeLast();
+    }
+}

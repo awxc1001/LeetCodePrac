7b220885493cb82dfdd19b6c6f5c184802c4add0
Andre Wang <110853339+awxc1001@users.noreply.github.com>
2023-05-25 02:56:50 +1000

    Time: 0 ms (100.00%), Space: 41.2 MB (97.82%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -14,32 +14,35 @@
  * }
  */
 class Solution {
+    
+//     Constraints:
+
+// The number of nodes in the tree is in the range [1, 1000].
+// Node.val is 0 or 1.
     int result = 0;
     int path = 0;
-
     public int sumRootToLeaf(TreeNode root) {
         traverse(root);
         return result;
     }
-
-    public void traverse(TreeNode root) {
-        if (root == null) {
+    
+    public void traverse(TreeNode root){
+        if(root == null){
             return;
         }
-
-        // Preorder record
-        path = (path << 1) | root.val;
-
-        // Hits leaf node, modify result
-        if (root.left == null && root.right == null) {
-            // Update result
-            result += path;
-        } else {
-            traverse(root.left);
-            traverse(root.right);
+        
+        //hits leaf node, modify result
+        if( root.left == null && root.right == null){
+            result += path << 1 | root.val;
+            // result += path;
+            return;
         }
-        // Postorder remove
+        
+        //preorder record
+        path = path << 1 | root.val;
+        traverse(root.left);
+        traverse(root.right);
+        //postorder remove
         path = path >> 1;
     }
 }
-

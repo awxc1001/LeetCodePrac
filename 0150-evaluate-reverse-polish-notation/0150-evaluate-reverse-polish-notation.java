31c998d76551b1141c0b8cf0343d35c489cb1fb4
Andre Wang <110853339+awxc1001@users.noreply.github.com>
2023-06-12 01:29:09 +1000

    Time: 7 ms (82.05%), Space: 43.5 MB (30.18%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -6,12 +6,14 @@ class Solution {
         for(String token: tokens){
             if("+-*/".contains(token)){
                 
+                //grab two numbers from top of the stack, last int first out
                 int a = stk.pop(); 
                 int b = stk.pop();
                 
+                //last in first out, so b-a and b/a b is the first number
                 switch(token){
                     case "+":
-                        stk.push(a+b);
+                        stk.push(b+a);
                         break;
                     case "-":
                         stk.push(b-a);
@@ -25,10 +27,11 @@ class Solution {
                 }
             }
             else{
+                //number just push in, can add if
                 stk.push(Integer.parseInt(token));
             }
         }
-        
+        //last number in the stack is the answer
         return stk.pop();
     }
 }

cc90e36031d768738a99d633810c564f35038d24
Andre Wang <110853339+awxc1001@users.noreply.github.com>
2023-06-12 03:44:53 +1000

    Time: 1 ms (96.45%), Space: 40.6 MB (71.99%) - LeetHub

---
+++ 0362-design-hit-counter/0362-design-hit-counter.java
@@ -0,0 +1,30 @@
+class HitCounter {
+    
+    //counts the number of hits received in the past 5 minutes 
+    Queue<Integer> q = new LinkedList<>();
+
+    public HitCounter() {
+        
+    }
+    
+    public void hit(int timestamp) {
+        q.offer(timestamp);
+    }
+    
+    public int getHits(int timestamp) {
+        //first in first out, so we can see if it past 300 seconds
+        while( !q.isEmpty() && timestamp - q.peek() >= 300){
+            //removed all the invalid ones from q
+            q.poll();
+        }
+        //remaining size is the total hits within 5 mins
+        return q.size();
+    }
+}
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */

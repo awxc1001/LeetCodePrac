6e40af8dec52c268049abc51748819005f1287b6
Andre Wang <110853339+awxc1001@users.noreply.github.com>
2023-06-27 03:11:38 +1000

    Time: 3 ms (75.40%), Space: 46.6 MB (25.19%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,26 +1,35 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
         
-        int left = 0;
-        int right = 0;
+        //keep a valid window by only allowing k 0s
+        // if k =2, then 1,0,0 is the maximum condition
         
-        int maxLen = 0;
+        int left =0;
+        int right=0;
+        
+        //this is a zeroCount for the sliding window
         int zeroCount =0;
-
+        int maxLen = Integer.MIN_VALUE;
         while(right < nums.length){
+            
             if(nums[right] == 0){
                 zeroCount++;
             }
+            
             right++;
-
-            while(zeroCount > k){
-                if(nums[left] == 0) {
+            
+            //if now window is 10010, need to remove the zeroCount to 2 if k=2
+            while( zeroCount> k ){
+                if(nums[left] == 0){
                     zeroCount--;
                 }
                 left++;
             }
-            maxLen = Math.max(maxLen, right - left);
+           //as long zeroCount is valid, return the maxinum number of 
+            maxLen = Math.max(maxLen, right-left);
+            
         }
+        
         return maxLen;
     }
 }

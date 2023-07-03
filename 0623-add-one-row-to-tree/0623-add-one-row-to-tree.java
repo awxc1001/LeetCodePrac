be73a5b167d3f071c87bc042a9b6b8aa83684195
Andre Wang <110853339+awxc1001@users.noreply.github.com>
2023-07-03 19:41:19 +1000

    Time: 0 ms (100.00%), Space: 43.6 MB (58.66%) - LeetHub

--- 0623-add-one-row-to-tree/0623-add-one-row-to-tree.java
+++ 0623-add-one-row-to-tree/0623-add-one-row-to-tree.java
@@ -14,39 +14,50 @@
  * }
  */
 class Solution {
+    int dep = 0;
+    
     public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        //The number of nodes in the tree is in the range [1, 104].
+    //the depth of the tree is in the range [1, 104].
         if(depth == 1){
+    //if depth == 1 that means there is no depth depth - 1 at all, then create a tree node  with value val as the new root of the whole original tree, and the original tree is the new root's left subtree.
             TreeNode newRoot = new TreeNode(val);
             newRoot.left = root;
             return newRoot;
         }
         
-        traverse(root, val, depth, 1);
+        traverse(root,val,depth);
         return root;
     }
     
-    public void traverse(TreeNode root, int val, int depth, int currentDepth){
-        if(root == null){
-            return;
-        }
-        
-        if(currentDepth == depth - 1){
-            TreeNode left = root.left;
-            TreeNode right = root.right;
-            root.left = new TreeNode(val);
-            root.right = new TreeNode(val);  
-            root.left.left = left;
-            root.right.right = right; 
-            return; 
-        }
-        traverse(root.left, val, depth, currentDepth + 1);
-        traverse(root.right, val, depth, currentDepth + 1);
+    public void traverse(TreeNode root, int val, int depth){
+    if(root == null){
+        return;
     }
+
+    //entering a node
+    dep++;
+
+    if(dep == depth-1){
+        //original left and right subtree
+       TreeNode left = root.left;
+       TreeNode right = root.right;
+       //assign new ones 
+       root.left = new TreeNode(val);
+       root.right = new TreeNode(val);  
+       root.left.left = left;
+       root.right.right = right;
+       //reach depth
+       //No need to reset dep here. dep should decrease as we go back to upper level.
+    } else {
+       //traverse
+       traverse(root.left,val,depth);
+       traverse(root.right,val,depth);
+    }
+    //postOrder, go back to upper level, so decrease dep
+    dep--;
 }
 
+    
 
-
-
-
-
-
+}
